0cb92d197bcb17dfe5e00ce88c7109f9e088ead4
gartee-john-PFG <gartee.john@principal.com>
2021-04-07 14:40:54 -0400

    update instructions in code files

--- fizzbuzz/lib/src/main/java/fizzbuzz/FizzBuzz.java
+++ fizzbuzz/lib/src/main/java/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,12 @@
  */
 package fizzbuzz;
 
+// call a method with the signature "String exec(int number)" in the class FizzBuzz
+// if 'number' is divisible by 3 return "Fizz"
+// if 'number' is divisible by 5 return "Buzz"  
+// if 'number' is divisible by 3 and by 5 return "FizzBuzz"
+// otherwise, return the string representation of 'number'  
+
 public class FizzBuzz {
     public boolean someLibraryMethod() {
         return true;
--- fizzbuzz/lib/src/test/java/fizzbuzz/FizzBuzzTest.java
+++ fizzbuzz/lib/src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -6,6 +6,27 @@ package fizzbuzz;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+//  Rules of TDD: (Transforms that change behavior w/o changing structure)
+//  1.  You are not allowed to write production code unless it 
+//      is to make a failing test pass
+//  2.  You are not allowed to write any more of a unit test 
+//      than is sufficient to fail...compile errors count as failing.
+//  3.  You are not allowed to write any more code than 
+//      required to make the test pass
+
+//  Rules of refactoring (Refactors change structure w/o changing behavior)
+//  1.  You are not allowed to refactor unless all of your tests are green (or disabled/skipped)
+//  2.  You are not allowed to change any behavior
+//  3.  Run your tests on EVERY code change
+
+
+//  tips
+//  use well-named tests
+//  don't write unnecessary tests
+//  make the smallest possible changes
+//  follow THINK, RED, GREEN, REFACTOR
+//  run your tests every time you touch your code
+
 class FizzBuzzTest {
     @Test void testSomeLibraryMethod() {
         FizzBuzz fb = new FizzBuzz();
